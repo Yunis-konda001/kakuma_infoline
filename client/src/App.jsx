6641b2de29890c3ca255ca5c4b7a853325e8965a
Yunis-Konda001 <k.konda@alustudent.com>
2025-07-17 20:09:36 +0300

    sg

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './HomePage';
+import SettlementSelectionPage from './SettlementSelectionPage';
 import InformationPage from './InformationPage';
 import FieldPostOfficesPage from './FieldPostOfficesPage';
 import './App.css'
@@ -9,7 +10,8 @@ function App() {
     <Router>
       <Routes>
 
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<SettlementSelectionPage />} />
+          <Route path="/home" element={<HomePage />} />
           <Route path="/information" element={<InformationPage />} />
           <Route path="/field-post-offices" element={<FieldPostOfficesPage />} />
       </Routes>
---
+++ client/src/SettlementSelectionPage.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './SettlementSelectionPage.css';
+
+function SettlementSelectionPage() {
+  return (
+    <div className="settlement-selection-page">
+      <div className="logo">
+        Kakuma<span className="highlight">InfoLine</span>
+      </div>
+      <p className="info-text">
+        Information for asylum-seekers, refugees, and migrants in Kakuma Camp.
+        Please select your settlement to continue.
+      </p>
+      <div className="location-buttons">
+        <Link to="/home" className="location-button">
+          Kakuma
+        </Link>
+        <button className="location-button disabled" disabled>
+          Kalobeyei (Coming Soon)
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default SettlementSelectionPage;
